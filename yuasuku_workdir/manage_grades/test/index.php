66f7b752dc20701ac4dee3de3465cef737de105f
松永誠 <ojpmwjpdj8jmt65mtofcijw@gamil.com>
2024-01-31 22:28:51 +0900

    中級の成績管理アプリに取り掛かりました。プロトタイプのchapter3/1-gまで終わらせる。一応crud処理ができました。

---
+++ yuasuku_workdir/manage_grades/test/index.php
@@ -0,0 +1,31 @@
+<?php require('../dbconnect.php'); ?>
+<!doctype html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap Css -->
+	 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<title></title>
+	</head>
+
+	<body>
+		<?php
+		$tests = $db->query('select * from tests');
+?>
+		<?php while ($test = $tests->fetch()): ?>
+		<?php
+		print($test['id'] . ' ' . $test['test_name'] . ' ' . $test['created_at'] . ' ' . $test['modified_at']);
+		?>
+		<a href="edit.php?id=<?php echo $test['id']; ?>">編集する</a>
+		<a href="delete.php?id=<?php echo $test['id']; ?>">削除する</a>
+<?php echo '<br>'; ?>
+		
+<?php endwhile; ?>
+		<a href="create.php">作成する</a>
+		<br>
+	
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	</body>
+</html>	
---
+++ yuasuku_workdir/manage_grades/test/save.php
@@ -0,0 +1,25 @@
+<?php require('../dbconnect.php');
+
+$tests = $db->prepare('INSERT INTO tests SET test_name=?, created_at=NOW()');
+$tests->execute(array($_POST['test_name']));
+echo 'テストの登録が完了しました';
+
+
+?>
+<!doctype html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap Css -->
+	 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<title></title>
+	</head>
+
+	<body>
+
+		<a href="index.php">戻る</a>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	</body>
+</html>	
