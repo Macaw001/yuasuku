12a150ce88d0fa7ac8c3a42038348d6198db4101
松永誠 <ojpmwjpdj8jmt65mtofcijw@gamil.com>
2024-02-09 22:18:23 +0900

    生徒の個人ページにすべての項目のグラフを実装。色がすべて同じなので要改善

--- yuasuku_workdir/manage_grades/student/student.php
+++ yuasuku_workdir/manage_grades/student/student.php
@@ -11,7 +11,7 @@ $students->execute(array($_REQUEST['student_id']));
 $student = $students->fetch();
 $student_name = $student['name'];
 
-$column_names = ['test_name'=>'テスト名', 'japanese'=>'国語', 'english'=>'英語', 'science'=>'社会', 'mathematics'=>'数学', 'sum'=>'合計'];
+$column_names = ['test_name'=>'テスト名', 'japanese'=>'国語', 'english'=>'英語', 'science'=>'理科', 'society'=>'社会', 'mathematics'=>'数学', 'sum'=>'合計'];
 ?>
 <!doctype html>
 <html lang="ja">
@@ -47,18 +47,33 @@ $column_names = ['test_name'=>'テスト名', 'japanese'=>'国語', 'english'=>'
 				<?php endwhile; ?>
 
 		</table>
+		<!-- forループで横の項目文キャンバスを作る -->
+		<?php for($num = 1; $num < count($column_names); $num++): ?>
+		<div style="width: 800px;"><canvas id="bar_graph<?php echo $num; ?>"></canvas></div>
+		<?php echo $num; ?>
+		<?php endfor; ?>
 		</div>
-		
 
-		<div style="width: 800px;"><canvas id="bar_graph"></canvas></div>
-		
-		
-		
+
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 		<!-- chartjsでグラフを実装　-->
+
+
+
 		<script>
-		const ctx = document.getElementById('bar_graph');
+		const subjects = ['japanese', 'english', 'science', 'society', 'mathematics', 'sum'];
+		const subjects_num = subjects.length;
+		const graphs = [];
+		for (let i = 1; i <= subjects_num; i++) {		
+			graphs.push('bar_graph' + i);
+		}
+		const ctxs = [];
+		for (const graph of graphs) {
+			ctxs.push(document.getElementById(graph));
+		}
+		
+	
 	
 		//lebelsにclass="test_name"の要素を配列として取得する
 		const tableData = document.querySelectorAll(".test_name");
@@ -66,27 +81,59 @@ $column_names = ['test_name'=>'テスト名', 'japanese'=>'国語', 'english'=>'
 		for (const testname of tableData) {
 			labels.push(testname.textContent);
 		}
+		
 		//alert(labels[0]);
 
 		//sumの適用されたクラス内のデータを取得
-		const gokei = document.querySelectorAll(".sum");
-		const data = [];
-		for (const sum of gokei) {
-			data.push(sum.textContent);
+		const class_subjects = ['.japanese', '.english', '.science', '.society', '.mathematics', '.sum'];
+		const contents = [];
+		const array_data = [];
+		for (let i = 0; i < class_subjects.length ; i++) {
+			let data = [];
+			let currentContents = document.querySelectorAll(class_subjects[i]); //.japaneseなどのデータを取得
+			for (const content of currentContents) {
+				data.push(content.textContent.trim());
+			//	console.log(content.textContent);
+			//	console.log('test' + i);
+			//	console.log(data);
+			}
+
+			
+			console.log(currentContents);
+			array_data.push(data);
+			console.log(data);
+			data = [];
+			console.log(data);	
+			console.log(array_data);
+			console.log(i);
+			
 		}
 
+		const top_labels = ['国語', '英語', '理科', '社会', '数学', '合計'];
+
+		function create_chart(i_ctx ,num) {
+			new Chart(i_ctx, {
+                	type: 'bar',
+                	data: {
+                        	labels: labels, //labelsはtdのデータが入った配列
+                                	datasets: [{
+                                	label: top_labels[num],
+                                	data: array_data[num], //右のデータは合計のテーブル内のデータが入った配列
+                                	backgroundColor: '#f88'
+                			}]
+          			}
+                	});
+                }
+
+		
+
+
 		//キャンバスにグラフを描写
-		const myChart = new Chart(ctx, {
-		type: 'bar',
-		data: {
-			labels: labels,	//labelsはtdのデータが入った配列
-				datasets: [{
-				label: '合計',
-				data: data, //右のデータは合計のテーブル内のデータが入った配列
-				backgroundColor: '#f88'
-		}]
+		let i = 0;
+		for (const i_ctx of ctxs) {
+			create_chart(i_ctx ,i);
+			i++;
 		}
-		});
 
 		</script>	
 	</body>
